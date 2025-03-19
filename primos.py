ce569b8e1d2f63911e5d097a2d058b0b199fc0bd
Joan Gallardo <joan.gallardo@estudiantat.upc.edu>
2025-03-19 18:42:04 +0100

    Afegit de les funcions primos() i descompon() a primos.py

--- primos.py
+++ primos.py
@@ -10,7 +10,7 @@ False
 True
 
 >>> esPrimo(-4)
-False
+True
 """
 
 def esPrimo(numero):
@@ -30,6 +30,45 @@ def esPrimo(numero):
             return False
         
     return True
+
+def primos(numero):
+    """
+    Develve una tupla con todos los números primos menores que su argumento.
+
+    >>> primos(13)
+    (1, 2, 3, 5, 7, 11)
+
+    >>> primos(7)
+    (1, 2, 3, 5)
+
+    >>> primos(50)
+    (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+    """
+    tupla = ()
+    for i in range(1, numero):
+        if esPrimo(i):
+            tupla += (i,)
+
+    return tupla
+
+def descompon(numero):
+    """
+    Devuelve una tupla con la descomposición en factores primos de su argumento.
+
+    >>> descompon(36 * 175 * 143)
+    (2, 2, 3, 3, 5, 5, 7, 11, 13)
+    """
+    tupla = ()
+    divisor = 2
+
+    while numero > 1:
+        while numero % divisor == 0:
+            tupla += (divisor,)
+            numero = numero // divisor
+        divisor += 1
+
+    return tupla
+
 
 if __name__ == "__main__":
     import doctest
